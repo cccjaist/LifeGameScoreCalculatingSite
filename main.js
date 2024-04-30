9fd70394bcdfe0e5c076a4482d0ff44067ec1285
makabi-bmk <nojiko0101@gmail.com>
2024-04-30 12:23:42 +0900

    フォームに入力した値を基に表の値を更新できるようにした

--- main.js
+++ main.js
@@ -5,24 +5,28 @@ window.addEventListener("load", (event) => {
     for (let row of table.rows) {
         let cnum = 0;
         for(let cell of row.cells){
-        cell.id = rnum + "-" + cnum;
-        cnum++;
-        console.log(cell.innerText);
+            cell.id = rnum + "-" + cnum;
+            cnum++;
         }
         rnum++;
     }
-
-    
 });
 
 function changeStatus() {
     let nameNum = document.getElementsByName("name-num");
     let len = nameNum.length;
+    let targetNum = 0;
     
     for (let i = 0; i < len; i++) {
         if (nameNum.item(i).checked) {
-            console.log(nameNum.item(i).value);
+            targetNum = nameNum.item(i).value;
             break;
         }
+    }
+
+    for (let i = 0; i < 3; i++) {
+        let cell = document.getElementById((targetNum + 1) + "-" + (i + 2));
+        let inputValue = parseInt(document.getElementById("input-value-" + (i + 1)).value);
+        cell.innerText = parseInt(cell.innerText) + inputValue;
     }
 }
