da5cac346690c27220ad66891bd71d7e97160a1c
makabi-bmk <nojiko0101@gmail.com>
2024-05-01 16:18:53 +0900

    判定ボタンを押したらランキングのダイアログが表示されるようにした

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const charactersName = ["荒井 マリカ", "高橋 和美", "新庄 絵里", "山本 かおる", "奈良 林凜"];
+
 window.addEventListener("load", (event) => {
     let table = document.getElementById("calc-table");
 
@@ -34,15 +36,49 @@ function changeStatus() {
 }
 
 function judge() {
-    let score = [0, 0, 0, 0, 0];
+    // let score = [0, 0, 0, 0, 0];
+    let score = {};
     let weight = [1, 1/5, 1/5000];
-    for (let i = 0; i < 5; i++) {
+    let len = charactersName.length;
+
+    for (let i = 0; i < len; i++) {
+        score[charactersName[i]] = 0;
+    }
+
+    for (let i = 0; i < len; i++) {
         for (let j = 0; j < 3; j++) {
             let cell = document.getElementById((i + 1) + "-" + (j + 2));
-            score[i] += weight[j] * parseInt(cell.innerText);
+            score[charactersName[i]] += weight[j] * parseInt(cell.innerText);
         }
     }
 
-    console.log(score);
+    let sortedScore = Object.keys(score).map((k)=>({key:k, value:score[k]}));
+    sortedScore.sort((a, b)=> b.value - a.value);
 
+    let ranking = document.getElementById("ranking");
+    let rankingText = "";
+    let rankNum = 1;
+
+    for (let i = 0; i < len; i++) {
+        rankingText += rankNum + "位. " + sortedScore[i]["value"].toFixed(2) + "点：" + sortedScore[i]["key"] + "\n";
+        if (i < len - 1 && sortedScore[i]["value"] != sortedScore[i + 1]["value"]) {
+            rankNum++;
+        }
+    }
+
+    ranking.innerText = rankingText;
+
+    let dialog = document.getElementById("result-dialog");
+    dialog.style.visibility = "visible";
+
+    let back = document.getElementById("dialog-background");
+    back.style.visibility = "visible";
+}
+
+function hiddenDialog() {
+    let dialog = document.getElementById("result-dialog");
+    dialog.style.visibility = "hidden";
+
+    let back = document.getElementById("dialog-background");
+    back.style.visibility = "hidden";   
 }
